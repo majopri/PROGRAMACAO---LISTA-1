0a0850c415cfcaad4942c1c3cd2da2acf67c713e
majopri <166362054+majopri@users.noreply.github.com>
2024-08-13 12:32:07 -0300

    Lista 1 - Ex 11

---
+++ EX 11 Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double x;
+            double y;
+            double ResultadoPotencia;
+
+            Console.Write("Digite o valor de x para calcular sua potência:  ");
+            x = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de y para ser o expoente:  ");
+            y = double.Parse(Console.ReadLine());
+
+            ResultadoPotencia = Math.Pow(x, y);
+
+            Console.Write("O Resultado da Potência é:  ");
+            Console.WriteLine(ResultadoPotencia);
+
+
+
+        }
+    }
+}
