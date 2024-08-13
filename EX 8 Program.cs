f1512529bd17fd04a11fb320dcf87b24f74aa148
majopri <166362054+majopri@users.noreply.github.com>
2024-08-13 12:30:10 -0300

    Lista 1 - Ex 8

---
+++ EX 8 Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius;
+            double fahrenheit;
+
+
+            Console.Write("Digite a temperatura em Celsius: ");
+            celsius = double.Parse(Console.ReadLine());
+
+            fahrenheit = (celsius * 9 / 5) + 32;
+
+            Console.Write("O valor dado em Celsius transformado em fahrenheit equivale a:  ");
+            Console.WriteLine(fahrenheit);
+
+        }
+    }
+}
