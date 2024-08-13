8d0756cf293ff9809676c856044e596058eab365
majopri <166362054+majopri@users.noreply.github.com>
2024-08-13 12:30:47 -0300

    Lista 1 - Ex 9

---
+++ EX 9 Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double diamentro;
+            double area;
+
+            Console.Write("Digite o valor do diâmentro do Círculo:  ");
+            diamentro = double.Parse(Console.ReadLine());
+
+
+            area = Math.Pow( diamentro/2,2) * 3.14;
+
+            Console.Write("O valor da area do círculo é:  ");
+            Console.WriteLine(area);
+        }
+    }
+}
